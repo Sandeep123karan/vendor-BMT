12a58ac7f0dbdcceb3abd1d42d09ebc0a001ce7f
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-07 17:43:09 +0530

    Update Vendor.model.js

--- models/Vendor.model.js
+++ models/Vendor.model.js
@@ -2,9 +2,29 @@ const mongoose = require("mongoose");
 
 const vendorSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    service: {
+      type: String,
+      enum: ["cab", "bus", "train", "homestay", "hotel"],
+      required: true
+    }
   },
   { timestamps: true }
 );
