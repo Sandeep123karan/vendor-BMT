db4db3fb7dab436368e9e74af870f2bebae60031
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-23 13:46:42 +0530

    Update Vendor.model.js

--- models/Vendor.model.js
+++ models/Vendor.model.js
@@ -1,3 +1,35 @@
+// const mongoose = require("mongoose");
+
+// const vendorSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true
+//     },
+
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true
+//     },
+
+//     password: {
+//       type: String,
+//       required: true
+//     },
+
+//     service: {
+//       type: String,
+//       enum: ["Cabs", "Buses", "train", "homestay", "hotel","Stay","Holiday Packages","Entertainment","Cruise","Buses","Cabs","Tour & Activities","BMT Darshan"],
+//       required: true
+//     }
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Vendor", vendorSchema);
 const mongoose = require("mongoose");
 
 const vendorSchema = new mongoose.Schema(
@@ -12,7 +44,8 @@ const vendorSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      lowercase: true
+      lowercase: true,
+      trim: true
     },
 
     password: {
@@ -22,8 +55,10 @@ const vendorSchema = new mongoose.Schema(
 
     service: {
       type: String,
-      enum: ["Cabs", "Buses", "train", "homestay", "hotel","Stay","Holiday Packages","Entertainment","Cruise","Buses","Cabs","Tour & Activities","BMT Darshan"],
-      required: true
+      enum: ["cabs", "buses", "train", "homestay", "hotel", "bmt darshan"],
+      required: true,
+      lowercase: true,
+      trim: true
     }
   },
   { timestamps: true }
